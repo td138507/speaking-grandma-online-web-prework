b8359be6ae64e4bd7a233386370adc3bbadfea59
Taylor Doyle <tcd56@cornell.edu>
2019-04-09 03:03:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,13 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+def speak_to_grandma
+  if "hi"=="HI"
+    puts "HUH?! SPEAK UP, SONNY!"
+  elsif "shout"=="no"
+    puts "NO, NOT SINCE 1998!"
+  elsif "I LOVE YOU GRANDMA!"="I LOVE YOU GRANDMA!"
+    puts "I LOVE YOU TOO PUMPKIN!"
+end
+  
+  
